67fd8652b7f4e0f6ca48f3d4f1324705ebebc334
Kenobi02 <andreseddyrijo@outlook.com>
2025-04-27 09:29:06 -0400

    Update script.js

--- CartaPDF-main/script.js
+++ CartaPDF-main/script.js
@@ -1,13 +1,13 @@
 document.querySelector('.heart').addEventListener('click', function() {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'Galaxia.pdf', true); // Asegúrate que el nombre del archivo sea correcto.
+    xhr.open('GET', 'foto.pdf', true); // Asegúrate que el nombre del archivo sea correcto.
     xhr.responseType = 'blob'; 
     xhr.onload = function() {
       if (xhr.status === 200) {
         const blob = xhr.response;
         const link = document.createElement('a');
         link.href = window.URL.createObjectURL(blob);
-        link.download = 'mi_gatita_hermosa.pdf'; // Nombre con el que se descarga la carta
+        link.download = 'mirando_el_cielo.pdf'; // Nombre con el que se descarga la carta
         link.click();
         window.URL.revokeObjectURL(link.href);
 
