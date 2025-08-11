4d168a80a412110b9386aa5ad2b212eb096e9e0c
CodeZero <andreseddyrijo@outlook.com>
2025-08-11 19:55:18 -0400

    Update script.js

--- CartaPDF-main/script.js
+++ CartaPDF-main/script.js
@@ -1,13 +1,13 @@
 document.querySelector('.heart').addEventListener('click', function() {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'carta.pdf', true); // Asegúrate que el nombre del archivo sea correcto.
+    xhr.open('GET', 'memorias.pdf', true); // Asegúrate que el nombre del archivo sea correcto.
     xhr.responseType = 'blob'; 
     xhr.onload = function() {
       if (xhr.status === 200) {
         const blob = xhr.response;
         const link = document.createElement('a');
         link.href = window.URL.createObjectURL(blob);
-        link.download = 'Universo.png'; // Nombre con el que se descarga la carta
+        link.download = 'memorias_de_mi_amor.png'; // Nombre con el que se descarga la carta
         link.click();
         window.URL.revokeObjectURL(link.href);
 
