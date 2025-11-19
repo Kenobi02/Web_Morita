6dbbf16adafbc5c0dae807e342d203ce03d83e5e
CodeZero <andreseddyrijo@outlook.com>
2025-11-18 22:20:14 -0400

    Update script.js

--- CartaPDF-main/script.js
+++ CartaPDF-main/script.js
@@ -1,6 +1,6 @@
 document.querySelector('.heart').addEventListener('click', function() {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'CartaNuevo', true); // Asegúrate que el nombre del archivo sea correcto.
+    xhr.open('GET', 'CartaNuevo.pdf', true); // Asegúrate que el nombre del archivo sea correcto.
     xhr.responseType = 'blob'; 
     xhr.onload = function() {
       if (xhr.status === 200) {
